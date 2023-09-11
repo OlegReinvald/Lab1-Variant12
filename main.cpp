c0acf34c066df405d28d409abd8ccd33bafb8651
Oleg <olejatbwp@gmail.com>
2023-09-11 13:45:11 +0300

    Works for 5 digits

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main() {
+    double x;
+    cin >> x;
+    double result;
+    double temp = 1;
+    unsigned int factorial = 1;
+    unsigned int counter = 0;
+    while (temp >= pow(10, -5))
+    {
+        temp = (pow(x, counter)) / factorial;
+        result += temp;
+        counter += 1;
+        factorial *= counter;
+        counter += 1;
+        factorial *= counter;
+    }
+    double answ = (exp(x) + exp(-x)) / 2.0;
+    cout << setprecision(15) << result << "\n" << answ;
+    return 0;
+}
