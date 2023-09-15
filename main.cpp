9bdf234e73dde4d45f763628b028fced3ff12de8
Oleg <olejatbwp@gmail.com>
2023-09-15 18:27:41 +0300

    Final

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     double x;
     cin >> x;
-    double result;
+    long double result;
     long double temp = 1;
     long double factorial = 1;
     int counter = 0;
@@ -16,6 +16,9 @@ int main() {
             break;
         }
         temp = (pow(x, counter)) / factorial;
+        if (isinf(result + temp)) {
+            break;
+        }
         result += temp;
         counter += 1;
         factorial *= counter;
@@ -25,9 +28,8 @@ int main() {
         {
             break;
         }
-        cout << counter << " . " << pow(x, counter) << " . " << temp << " . " << factorial << "\n";
     }
-    long double answ = (exp(x) + exp(-x)) / 2.0;
-    cout << setprecision(20) << result << "\n" << answ;
+    long double answer = (exp(x) + exp(-x)) / 2.0;
+    cout << setprecision(20) << result << "\n" << answer;
     return 0;
 }
